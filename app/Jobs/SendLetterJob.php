491ec991c52c965d6f49018581a9d6e3c31315d4
ASt39 <alexandr.statut@gmail.com>
2024-03-30 11:41:17 +0200

    Modified: рефактор через Mail

--- app/Jobs/SendLetterJob.php
+++ app/Jobs/SendLetterJob.php
@@ -4,7 +4,6 @@ namespace App\Jobs;
 
 use App\Dto\NewUserDto;
 use App\Mail\UserCreated;
-use App\Notifications\EmailNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
